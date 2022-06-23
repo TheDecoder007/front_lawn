29405cf9d89171dda64773d77ac5b521b9d8238d
Ryan Murphy <TheDecoder007@gmail.com>
2022-06-23 12:42:53 -0400

    code from repository

--- Slice/src/game.ts
+++ Slice/src/game.ts
@@ -3,6 +3,7 @@ import { createInventory } from '../node_modules/decentraland-builder-scripts/in
 import Script1 from "../ff9257ec-9d62-404f-97c7-cf19c4035761/src/item"
 import * as utils from '@dcl/ecs-scene-utils'
 import { hud } from 'dcl-builder-hud'
+import { getUserData } from "@decentraland/Identity"
 
 
 const _scene = new Entity('_scene')
@@ -99,3 +100,42 @@ const script1 = new Script1()
 script1.init()
 script1.spawn(fantasyChest, {"onClickText":"Open/Close","onClick":[{"entityName":"fantasyChest","actionId":"toggle","values":{}}]}, createChannel(channelId, fantasyChest, channelBus))
 
+//CODE FOR MIRROR
+const stand = new Entity();
+stand.addComponent(new BoxShape());
+stand.addComponent(new Transform({ position: new Vector3(10, 0, 8) }));
+engine.addEntity(stand);
+
+const avatar = new Entity();
+const avatarShape = new AvatarShape();
+hud.attachToEntity(stand)
+
+
+avatarShape.bodyShape = "urn:decentraland:off-chain:base-avatars:BaseFemale";
+avatarShape.wearables = [
+  "urn:decentraland:off-chain:base-avatars:f_sweater",
+  "urn:decentraland:off-chain:base-avatars:f_jeans",
+  "urn:decentraland:off-chain:base-avatars:bun_shoes",
+  "urn:decentraland:off-chain:base-avatars:standard_hair",
+  "urn:decentraland:off-chain:base-avatars:f_eyes_00",
+  "urn:decentraland:off-chain:base-avatars:f_eyebrows_00",
+  "urn:decentraland:off-chain:base-avatars:f_mouth_00",
+];
+avatarShape.skinColor = new Color4(0.94921875, 0.76171875, 0.6484375, 1);
+avatarShape.eyeColor = new Color4(0.23046875, 0.625, 0.3125, 1);
+avatarShape.hairColor = new Color4(0.234375, 0.12890625, 0.04296875, 1);
+avatar.addComponent(avatarShape);
+avatar.addComponent(new Transform({ position: new Vector3(10, 0.5, 8) }));
+engine.addEntity(avatar);
+
+
+void getUserData().then(async a => {
+  const res = await fetch(`https://peer.decentraland.org/lambdas/profiles/${a?.publicKey}`)
+  const json = await res.json()
+  const av = json.avatars[0].avatar
+  avatarShape.bodyShape = av.bodyShape
+  avatarShape.skinColor = new Color4(av.skin.color.r, av.skin.color.g, av.skin.color.b, 1);
+  avatarShape.eyeColor = new Color4(av.eyes.color.r, av.eyes.color.g, av.eyes.color.b, 1);
+  avatarShape.hairColor = new Color4(av.hair.color.r, av.hair.color.g, av.hair.color.b, 1);
+  avatarShape.wearables = av.wearables
+})
